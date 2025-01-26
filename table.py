6d80de7c638f082511044c1d1075141362c161ef
Lara <Lara.pirk@icloud.com>
2025-01-26 12:26:30 +0000

    table push

--- table.py
+++ table.py
@@ -8,12 +8,48 @@ from datetime import datetime
 class Leaderboard:
     """hello"""
     score_table = {}
+    team_table = {}
+    team_scores_table = {}
 
     def set_score(self, name, score, timestamp=None, ):
         if timestamp is None:
             timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         self.score_table[name] = {'score': score, 'timestamp': current_time}
+
+    def set_team(self, name, team, timestamp=None):
+        if timestamp is None:
+            timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        self.team_table[name] = {'team': team, 'timestamp': current_time}
+
+
+        if name in self.score_table:
+            player_score = self.score_table[name]['score']
+            if team in self.team_scores_table:
+                self.team_scores_table[team] += player_score
+            else:
+                self.team_scores_table[team] = player_score
+
+    def save_team_scores_to_file(self, filename):
+
+        to_save = {
+            'team_table': self.team_table,
+            'team_scores_table': self.team_scores_table
+        }
+        with open(filename, 'w') as file:
+            json.dump(to_save, file)
+
+    def load_team_scores_from_file(self, filename):
+        try:
+            with open(filename, 'r') as file:
+                data = json.load(file)
+                self.team_table = data.get('team_table', {})  # Load team_table or default to empty
+                self.team_scores_table = data.get('team_scores_table', {})  # Load team_scores_table or default to empty
+        except FileNotFoundError:
+            # Handle the case where the file doesn't exist
+            self.team_table = {}
+            self.team_scores_table = {}
 
     def get_sorted_Scores(self, count, sort_order='descending'):
         if sort_order in ['ascending', 'descending']:
@@ -28,7 +64,23 @@ class Leaderboard:
         else:
             print("Invalid sort order. Use 'ascending' or 'descending'.")
             return []
+    def make_teams(self):
+        team_scores = {}
+        for name, team_info in self.team_table.items():
+            team_name = team_info['team']
+            if name in self.score_table:
+                player_score = self.score_table[name]['score']
+                if team_name in team_scores:
+                    team_scores[team_name] += player_score
+                else:
+                    team_scores[team_name] = player_score
+        return team_scores
 
+    def get_sorted_team_scores(self,sort_order='descending'):
+        team_scores = self.make_teams()
+
+        sorted_teams = sorted(team_scores.items(), key=lambda item: item[1],reverse=(sort_order=='descending'))
+        return sorted_teams
     def save_to_file(self, filename):
         with open(filename, 'w') as file:
             json.dump(self.score_table, file)
@@ -44,10 +96,11 @@ class Leaderboard:
         self.score_table = {}
         self.save_to_file(filename)
 
-
+team_leaderboard = Leaderboard()
 the_leaderboard = Leaderboard()
-the_leaderboard.load_from_file("leaderboard.json")
 
+the_leaderboard.load_from_file("leaderboard.json")
+team_leaderboard.load_team_scores_from_file("sorted_team_scores.json")
 
 class Handler(BaseHTTPRequestHandler):
     def do_POST(self):
@@ -58,10 +111,15 @@ class Handler(BaseHTTPRequestHandler):
 
             name = parsed_data.get("name", [""])[0]
             score = parsed_data.get("score", [""])[0]
+            team = parsed_data.get("team", [""])[0]
 
             if name and score.isdigit():
                 the_leaderboard.set_score(name, int(score))
                 the_leaderboard.save_to_file("leaderboard.json")
+
+            if name and team:
+                the_leaderboard.set_team(name, team)
+                the_leaderboard.save_team_scores_to_file("sorted_team_scores.json")
 
         if self.path == '/reset-leaderboard':
             the_leaderboard.clear_leaderboard("leaderboard.json")
@@ -99,7 +157,19 @@ class Handler(BaseHTTPRequestHandler):
         self.end_headers()
 
     def do_GET(self):
-        if self.path == '/download-sorted-scores':
+        if self.path == '/download-team-scores':
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.send_header('Content-Disposition',
+                             'attachment; filename="sorted_team_scores.json"')
+            self.end_headers()
+
+
+            sorted_team_scores = the_leaderboard.get_sorted_team_scores('descending')
+            self.wfile.write(json.dumps(sorted_team_scores).encode('utf-8'))
+
+
+        elif self.path == '/download-sorted-scores':
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
             self.send_header('Content-Disposition',
@@ -107,6 +177,8 @@ class Handler(BaseHTTPRequestHandler):
             self.end_headers()
 
             self.wfile.write(json.dumps(the_leaderboard.get_sorted_Scores(100, 'descending')).encode('utf-8'))
+
+
         else:
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
@@ -134,6 +206,8 @@ tr{padding-left: 1em; background-color: #f2f2f2; vertical-align: middle;}
                     <input type="text" id="name" name="name" required><br><br>
                     <label for="score">Score:</label>
                     <input type="number" id="score" name="score" required><br><br>
+                    <label for="team">Team:</label>
+                    <input type="text" id="team" name="team" required><br><br>
                     <input type="submit" value="Submit">
                 </form>
                 <form method="POST" action="/reset-leaderboard" style="margin-top: 1em;"><button type="submit" class="btn btn-danger">Clear Leaderboard</button>
@@ -144,8 +218,10 @@ tr{padding-left: 1em; background-color: #f2f2f2; vertical-align: middle;}
                 <table border="0">
                     <tr style="margin-bottom:1ex;"><th >Name</th><th style="padding-right:1ex;">Score</th><th style="padding-right:1ex;">Last Edited</th><th style="padding-left:2em;width:10em">bar</th></tr>
             """
-
+            sorted_team_scores = the_leaderboard.get_sorted_team_scores('descending')
             sorted_scores = the_leaderboard.get_sorted_Scores(100, 'descending')
+
+
             if sorted_scores:
                 min_score = sorted_scores[-1][1]
                 max_score = sorted_scores[0][1]
@@ -157,13 +233,32 @@ tr{padding-left: 1em; background-color: #f2f2f2; vertical-align: middle;}
                     color = f"hsl({hue}, 100%, 80%)"
 
                     html += f'<tr style=""><td>{name}</td><td>{score}</td><td>{timestamp}</td><td style="">      <div class="progress">        <div class="progress-bar" role="progressbar" aria-valuenow="{score}" aria-valuemin="0" aria-valuemax="{max_score}" style="background-color: {color}; width: {score * 100 / max_score}%;"><span class="sr-only">60% Complete</span></div>      </div></td></tr>'
+
+
+
             else:
                 html += "<tr><td colspan='3'>No scores yet.</td></tr>"
+
+
+            if sorted_team_scores:
+                html += """
+                        <h2> Team Scores</h2>
+                        <table border="0">
+                            <tr><th>Team</th><th>Score</th></tr>
+                """
+                for team_name, team_score in sorted_team_scores:
+                    html += f"<tr><td>{team_name}</td><td>{team_score}</td></tr>"
+                html += "</table>"
+            else:
+                html += "<h2>Team Scores</h2><p>No teams yet.</p>"
+
+
 
             html += """
                 </table>
                 <br>
                 <a href="/download-sorted-scores"><button type="button">Download Leaderboard</button></a>
+                <a href="/download-team-scores"><button type="button">Download Team Scores</button></a>               
                 <form action="/table-fileupload" method="post" enctype="multipart/form-data">
                     <input type="file" id="myFile" name="filename">
                     <input type="submit" value="Upload">
